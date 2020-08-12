aa7f43641c977179b0b4d495cf46f50ae122531b
unknown <mnzeevi@gmail.com>
2020-08-12 17:52:28 +0300

    hello world

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const qouteContainer = document.getElementById('quote-container')
-const qouteText = document.getElemhentById('qoute')
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const paryBtn = document.getElementById('twitter')
 const newQouteBtn = document.getElementById('new-quote')
@@ -9,13 +9,13 @@ const newQouteBtn = document.getElementById('new-quote')
 
 
 async function getQuote(){
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.comh/api/1.0/?method=getQuote&lang=en&format=json';
+    const proxyUrl = 'https://obscure-earth-96157.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl)
         const data = await response.json();
-        authorText.innerText = data.qouteAuthor;
-        qouteText.innerText = data.qouteText;
+        authorText.innerText = data.quoteAuthor;
+        quoteText.innerText = data.quoteText;
         console.log(data)
     }     
      catch(error){
