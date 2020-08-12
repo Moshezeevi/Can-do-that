7024498d440d4eb2f374f767b2df4e931043da6e
unknown <mnzeevi@gmail.com>
2020-08-12 07:41:36 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const qouteContainer = document.getElementById('quote-container')
+const qouteText = document.getElementById('qoute')
+const authorText = document.getElementById('author')
+const paryBtn = document.getElementById('twitter')
+const newQouteBtn = document.getElementById('new-quote')
+
+
+// get quote from api
+
+
+async function getQuote(){
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl)
+        const data = await response.json();
+        authorText.innerText = data.qouteAuthor;
+        qouteText.innerText = data.qouteText;
+        console.log(data)
+    }     
+     catch(error){
+         getQuote();
+    }
+}
+
+// on load
+
+getQuote();
