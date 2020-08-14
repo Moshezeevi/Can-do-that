da08dbaf3f93a26904f511e223970f67b4159fa2
unknown <mnzeevi@gmail.com>
2020-08-14 09:53:56 +0300

    master

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const newQouteBtn = document.getElementById('new-quote')
 
 
 async function getQuote(){
-    const proxyUrl = 'https://mighty-inlet-53536.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp';
+    const proxyUrl = 'https://git.heroku.com/obscure-earth-96157.git/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl)
         const data = await response.json();
