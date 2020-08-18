52f98a36fab135da57349caad783fe93d375da24
unknown <mnzeevi@gmail.com>
2020-08-18 13:38:21 +0300

    hello world

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ const newQouteBtn = document.getElementById('new-quote')
 
 
 async function getQuote(){
-    const proxyUrl = 'https://mighty-inlet-53536.herokuapp.com/'
+    const proxyUrl = 'https://obscure-earth-96157.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response = await fetch(proxyUrl + apiUrl)
+        const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
         authorText.innerText = data.quoteAuthor;
         quoteText.innerText = data.quoteText;
-        console.log(data)
-    }     
-     catch(error){
+        console.log(data);
+    }  catch(error){
          getQuote();
+         console.log('Whops, no quote', error)
     }
 }
 
